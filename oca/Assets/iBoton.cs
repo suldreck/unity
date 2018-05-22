34b7a310a64fa049c2fe4d8b3598b1759fc87cbc
suldreck <suldreck@hotmail.com>
2018-05-22 19:16:04 +0200

    test

---
+++ oca/Assets/iBoton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iBoton : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        vector = new int[4];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public int[] vector;
+    public int total;
+    public void fAleatorio()
+    {
+        
+
+        for (int i=0; i < vector.Length; i++)
+        {
+            vector[i] = Random.Range(1, 4);//1-3
+            Debug.Log("el valor es:_" + vector[i]);
+        }
+    }
+}
---
+++ oca/Assets/sficha.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sficha : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public  void OnMouseUp()
+    {
+        Debug.Log("click");
+    }
+}
