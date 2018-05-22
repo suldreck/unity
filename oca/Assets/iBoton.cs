95025a772a5556ec34d20c5bcfde9c10c6e8d7f4
suldreck <suldreck@hotmail.com>
2018-05-22 22:22:50 +0200

    imagenes
    manejo de arrays con imagenes

--- oca/Assets/iBoton.cs
+++ oca/Assets/iBoton.cs
@@ -9,8 +9,9 @@ public class iBoton : MonoBehaviour {
     {
         vector = new int[4];
     }
-    public Image[] imagenes1;
-    public Image[] imagenes0;
+    public Sprite[] imagenes0;
+    public Sprite[] imagenes1;
+    
     // Update is called once per frame
     void Update()
     {
@@ -24,8 +25,18 @@ public class iBoton : MonoBehaviour {
 
         for (int i=0; i < vector.Length; i++)
         {
-            vector[i] = Random.Range(1, 4);//1-3
+            vector[i] = Random.Range(0, 2);//1-3
             Debug.Log("el valor es:_" + vector[i]);
+            if(vector[i]==0)
+            {
+                this.transform.GetChild(i).GetComponent<Image>().sprite=imagenes0[ Random.Range (0, imagenes0.Length)];
+            }
+            else
+            {
+                Debug.Log("la longitud de imagenes1 es: " + imagenes0.LongLength);
+                Debug.Log("la longitud de imagenes1 es: " + imagenes1.LongLength);
+                transform.GetChild(i).GetComponent<Image>().sprite = imagenes1[Random.Range(0, imagenes1.Length)];
+            }
         }
     }
 }
