cd2fabca919d710751f83b47740f662f9452bc18
suldreck <suldreck@hotmail.com>
2018-05-23 17:13:55 +0200

    borja

--- oca/Assets/iBoton.cs
+++ oca/Assets/iBoton.cs
@@ -10,12 +10,16 @@ public class iBoton : MonoBehaviour {
         vector = new int[4];
     }
     public Sprite[] imagenes0;
-    
+    public bool dadoPulsado = false;//estado estado del dado que activara luego las acciones derivadas 
+    public void turnoSiguenteJugador()
+    {
+
+    }
     
     // Update is called once per frame
     void Update()
     {
-
+        dadoPulsado = false;
     }
     public int[] vector;
     public int total;
@@ -27,6 +31,8 @@ public class iBoton : MonoBehaviour {
         valorDado = Random.Range(0, imagenes0.Length);
         Debug.Log("el valor del dado es: " + valorDado);
         this.transform.GetChild(0).GetComponent<Image>().sprite = imagenes0[valorDado];
+        this.dadoPulsado = true;//hemos tirado el dado
+
        /* for (int i=0; i < vector.Length; i++)
         {
             vector[i] = Random.Range(0, 2);//1-3
---
+++ oca/Assets/usoValorDado.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class usoValorDado : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        tirada = GameObject.FindObjectOfType<iBoton>();
+    }
+    
+    iBoton tirada;
+    // Update is called once per frame
+    void Update () {
+        GetComponent<Text>().text = "="+tirada;
+        
+	}
+}
