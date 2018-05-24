22c5d2440f913bb2481312279e17ed896adc28f3
suldreck <suldreck@hotmail.com>
2018-05-24 12:37:48 +0200

    desacoplar casilla con ficha
    tengo q quitar el valor fijo para q la tirada sea siempre 2, en sficha, y ver el riggbi

--- oca/Assets/sficha.cs
+++ oca/Assets/sficha.cs
@@ -18,21 +18,37 @@ public class sficha : MonoBehaviour {
     public void OnMouseUp()
     {//ToDo,  asegurrse de que objeto ui, se esta pulsando, en este caso
         //deberia ser la ficha solamente.
-        Debug.Log("click");
-        int espaciosParaMover = dado.valorDado;
+      
+        Debug.Log(" ****posicion***" + this.transform.position);//para borrar
+        //int espaciosParaMover = dado.valorDado;
+        int espaciosParaMover = 2;//solo para testear
         Casilla casillaFinal = casillaActual;
+       // Debug.Log("la casilla siguiente " + casillaFinal.transform.position);// si es nullo
         for (int i = 0; i < espaciosParaMover; i++)
         {
-            if (casillaActual == null)
-            {
-                casillaFinal = casillaActual;
+            //Debug.Log("dentro del for");//se q entro en el for
+            if (casillaFinal == null)
+            {//*********recordar que cuando llegue a la 63, no tiene q ir a la casilla de inicio sino hacia atras.
+                casillaFinal = casillaInicio;
+                Debug.Log("casilla final es nula");// si es nullo
             }
             else
             {
-                casillaFinal = casillaActual.siguiente[0];//como si fuera un iterador
+                casillaFinal = casillaFinal.siguiente[0];//como si fuera un iterador
+                                                         //creo q no necesita ser un array,
+                                                  // por q solo puede ir a una casilla
+               // Debug.Log("la casilla siguiente "+ casillaFinal.transform.name);// si es nullo
             }
         }
-       
-       
+        if (casillaFinal == null)
+        {
+            return;
+        }
+
+        
+        this.transform.position = casillaFinal.transform.position;//revisar por q no entiendo
+        Debug.Log(" ---->posicion cambio<-----" + this.transform.position);
+
+        casillaActual = casillaFinal;
     }
 }
